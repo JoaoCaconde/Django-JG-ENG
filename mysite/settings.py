94b86d70622aa7ed6e0a5c03a478ee38cfd6f173
JoaoCaconde <joaoguilherme024@hotmail.com>
2026-01-23 14:54:23 -0300

    Deploy 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,5 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+MEDIA_ROOT = BASE_DIR / "uploads/"
+MEDIA_URL = "media/"
+
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+    "./uploads/media/project_images/",
+    "./portfolio/templates/portfolio/icons/",
+]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     projects = Post.objects.all()
@@ -7,3 +8,11 @@ def post_list(request):
         "projects": projects
     }
     return render(request, "portfolio/post_list.html", context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    project = Post.objects.get(pk=pk)
+    context = {
+        "project": project
+    }
+    return render(request, "portfolio/post_detail.html", context)
