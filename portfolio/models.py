a4563156ac1e12efef440f60893900928cad96dc
JoaoCaconde <joaoguilherme024@hotmail.com>
2026-01-29 11:52:25 -0300

    deploy WorkJob

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FileField(upload_to="media/project_images/", blank=True,max_length=10)
+    image = models.FileField(upload_to="media/project_images/", blank=True,max_length=100)
 
     def __str__(self):
         return self.title
