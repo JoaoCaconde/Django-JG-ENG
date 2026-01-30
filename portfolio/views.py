1c75153284cdf306da4914492c63ceda9bbdbe8b
JoaoCaconde <joaoguilherme024@hotmail.com>
2026-01-30 09:31:57 -0300

    pageDetail : + Carousel for multiple images per post

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, PostImage
 from django.shortcuts import render, get_object_or_404
 
-def post_list(request):
+"""def post_list(request):
     projects = Post.objects.all()
     context = {
         "projects": projects
@@ -15,4 +15,17 @@ def post_detail(request, pk):
     context = {
         "project": project
     }
-    return render(request, "portfolio/post_detail.html", context)
+    return render(request, "portfolio/post_detail.html", context)"""
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        "projects": posts
+    }
+    return render(request, 'portfolio/post_list.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    photos = PostImage.objects.filter(post=post)
+    project = Post.objects.get(pk=pk)
+    return render(request, 'portfolio/post_detail.html', {'post': post, 'photos': photos, 'project': project})
